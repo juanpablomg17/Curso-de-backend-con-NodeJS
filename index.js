88db2f86f091e52ea99368ea6ea59fab1a379e7e
juanpablomg17 <juanpablomg17@gmail.com>
2021-10-19 08:41:25 -0500

    feal: add my first routes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { application } = require("express");
 const express = require("express");
 const app = express();
 const port = 3001;
@@ -14,12 +15,46 @@ app.get("/categories", (req, res) =>{
 });
 
 app.get("/products", (req, res) =>{
-  res.json({
-    name: 'Product 1',
-    price: 1000
-  });
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000
+    },
+    {
+      name: 'Product 2',
+      price: 2000
+    },
+    {
+      name: 'Product 3',
+      price: 3000
+    }
+  ]);
   console.log(`Listening at http://localhost:${port}`);
 });
+
+
+app.get("/products/:id", (req, res)=> {
+
+  // el objeto req del callback, puede recibir los parámetros de la ruta, devuelve un objeto.
+
+  // esta sintaxis de es6 permite obtener sólo el parámetro id del objeto params
+  const {id} = req.params;
+  res.json({
+    id,
+    name: 'Product 3',
+    price: 3000
+  });
+});
+
+app.get("/categories/:categoryId/products/:productId", (req, res) => {
+    const {categoryId, productId} = req.params;
+    res.json({
+      categoryId,
+      productId,
+    })
+
+})
+
 
 app.listen(port, () =>{
   console.log("My port: " + port);
